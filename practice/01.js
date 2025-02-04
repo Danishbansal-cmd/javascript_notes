e3aab499fb55cb6e034d8a2728ef177f81992238
Danish Bansal <danishbansal@gmail.com>
2025-02-04 19:04:12 +0530

    added practice file

---
+++ practice/01.js
@@ -0,0 +1,94 @@
+// question first
+// function abc(){
+//     console.log(abc.xyz);
+// }
+// abc()
+// abc.xyz = 400
+// abc.xyz = 200
+// abc()
+
+// question second
+// const numbers = [1,2,3,4]
+// numbers[100] = 500
+// console.log(numbers)
+
+// question third
+// console.log(typeof typeof 100)
+
+// question fourth
+// const arr = [...'danish']
+// console.log(arr)
+
+// what does parseInt
+// question fifth
+// console.log(parseInt('10+2'))
+// console.log(parseInt('7FM'))
+// console.log(parseInt('M7F'))
+// console.log(typeof parseInt('M7F'))
+
+// 6th
+// console.log(isNaN('danish'))
+
+// 7th
+// console.log([1,2,3].map((number) => {
+//     if(number > 0){
+//         return
+//     }
+//     return number*2
+// }))
+
+// 8th
+// function newFun(){
+//     return;
+// }
+// newFun()
+// console.log(newFun())
+// console.log(dd)
+
+// 9th
+// console.log(typeof undefined)
+// console.log(typeof null)
+// console.log(typeof isNaN)
+// console.log(typeof Math)
+
+// 10th
+// 'use strict'
+// {
+// function tenth(){
+//     console.log('danish')
+// }
+// }
+// tenth()
+
+// 11th
+// var var1 = 'danish';
+// let let1 = 'bansal';
+// console.log(window.var1, window.let1) // danish undefined
+
+
+// 12th
+// function tweleth(a, b){
+//     'use strict'
+//     a = 100
+//     b = 200
+//     return arguments[0] + arguments[1]
+//     console.log(a+b)
+// }
+// console.log(tweleth(300,400))
+
+// 13th
+// what is mutable array
+
+// 14th
+// console.log([1,2,3,4,5,6,7].find(num => num > 4));
+// console.log([1,2,3,4,5,6,7].filter(num => num > 4));
+// console.log([1,2,3,4,5,6,7].find(num => num === 4));
+// console.log([1,2,3,4,5,6,7].filter(num => num === 4));
+// console.log([1,2,3,4,5,6,7].map(num => num > 4));
+
+// 15th
+// ques: is react a library or framework
+// ans: library
+
+// 16th
+// ques: what is diff framework vs library
